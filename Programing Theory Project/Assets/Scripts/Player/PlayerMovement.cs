1546220ba4963527fd31736baf29a55e5ec7b2dc
andriams <andriasergio@yahoo.fr>
2022-02-04 11:04:14 +0100

    add rotation feature

--- Programing Theory Project/Assets/Scripts/Player/PlayerMovement.cs
+++ Programing Theory Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _rotationSpeed = 10;
 
     private Rigidbody _playerRb;
     private float _horizontalInput;
     private float _verticalInput;
-    private float _speed = 10;
+    private float _orientationInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
+        GetInput();
     }
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        _orientationInput = Input.GetAxis("Orientation");
+    }
+
+    private void Move()
+    {
+        //translation
         _playerRb.velocity = new Vector3(_horizontalInput, 0, _verticalInput) * _speed;
+
+        //rotation
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, _orientationInput, 0) * _rotationSpeed * Time.fixedDeltaTime);
+        _playerRb.MoveRotation(_playerRb.rotation * deltaRotation);
     }
 }
